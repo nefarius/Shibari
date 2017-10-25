f2df4283ec3b4c73fcedc57bfab2b612cf23b9f3
Benjamin Höglinger <nefarius@dhmx.at>
2017-10-25 19:53:38 +0200

    Added ISinkPlugin

---
+++ Shibari.Sub.Core/Shared/Types/Common/Sinks/ISinkPlugin.cs
@@ -0,0 +1,11 @@
+﻿namespace Shibari.Sub.Core.Shared.Types.Common.Sinks
+{
+    public interface ISinkPlugin
+    {
+        void DeviceArrived(IDualShockDevice device);
+
+        void DeviceRemoved(IDualShockDevice device);
+
+        void InputReportReceived(IDualShockDevice device, IInputReport report);
+    }
+}
