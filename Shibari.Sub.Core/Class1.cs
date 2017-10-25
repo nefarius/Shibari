6e481bfa10217a3086fa447f31984023e0314a35
Benjamin Höglinger <nefarius@dhmx.at>
2017-10-25 17:04:17 +0200

    Add project files.

---
+++ Shibari.Sub.Core/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shibari.Sub.Core
+{
+    public class Class1
+    {
+    }
+}
