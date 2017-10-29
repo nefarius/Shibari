4ab186e507474e780ba5d3e569f8f424f6da64ea
Benjamin Höglinger <nefarius@dhmx.at>
2017-10-29 16:41:15 +0100

    Added listing of ViGEm

---
+++ Shibari.Dom.Driver.Installer/Util/ViGEmDevice.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using Nefarius.Devcon;
+
+namespace Shibari.Dom.Driver.Installer.Util
+{
+    public class ViGEmDevice
+    {
+        public static Guid ClassGuid => Guid.Parse("{96E42B22-F5E9-42F8-B043-ED0F932F014F}");
+
+        public static IEnumerable<ViGEmDevice> Devices
+        {
+            get
+            {
+                var list = new List<ViGEmDevice>();
+                var instance = 0;
+
+                while (Devcon.Find(ClassGuid, out var path, out var instanceId, instance++))
+                {
+                    list.Add(new ViGEmDevice() { DevicePath = path });
+                }
+
+                return list;
+            }
+        }
+
+        public string DevicePath { get; private set; }
+
+        public string Name => "Virtual Gamepad Emulation Bus";
+    }
+}
