1c7147bfff0800d72916412010331d126d03a6ea
Benjamin Höglinger <nefarius@dhmx.at>
2017-11-05 19:41:03 +0100

    Added DualShockDeviceCollection

---
+++ Shibari.Sub.Core/Shared/Types/Common/Collections/DualShockDeviceCollection.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.ObjectModel;
+using System.Linq;
+using System.Net.NetworkInformation;
+using Shibari.Sub.Core.Shared.IPC.Types;
+
+namespace Shibari.Sub.Core.Shared.Types.Common.Collections
+{
+    public class DualShockDeviceCollection : ObservableCollection<IDualShockDevice>
+    {
+        public IDualShockDevice this[PhysicalAddress address]
+        {
+            get { return this.First(d => d.ClientAddress.Equals(address)); }
+        }
+
+        public IDualShockDevice this[UniqueAddress address]
+        {
+            get { return this.First(d => new UniqueAddress(d.ClientAddress).Equals(address)); }
+        }
+    }
+}
