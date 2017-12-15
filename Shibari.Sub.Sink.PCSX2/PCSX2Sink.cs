486a9a32371f6063fbaa4573c8b047fb012e7fc3
Benjamin Höglinger <nefarius@dhmx.at>
2017-12-15 17:10:38 +0100

    Added Shibari.Sub.Sink.PCSX2

---
+++ Shibari.Sub.Sink.PCSX2/PCSX2Sink.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shibari.Sub.Sink.PCSX2
+{
+    class PCSX2Sink
+    {
+    }
+}
