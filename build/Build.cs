30d8032c3084f327ad2fed3d810bd83103744e8f
Benjamin Höglinger <nefarius@dhmx.at>
2018-01-01 23:34:08 +0100

    Revert "Added https://nuget.vigem.org/ to build script"
    This reverts commit b7211c35ce85133cc3f1742f6d9af0ff866b4501.

--- build/Build.cs
+++ build/Build.cs
@@ -1,12 +1,22 @@
-﻿using Nuke.Common.Tools.MSBuild;
-using Nuke.Core;
+﻿using System;
+using System.Linq;
+using Nuke.Common;
+using Nuke.Common.Git;
+using Nuke.Common.Tools.GitVersion;
+using Nuke.Common.Tools.MSBuild;
 using Nuke.Core.BuildServers;
+using Nuke.Core;
 using static Nuke.Common.Tools.MSBuild.MSBuildTasks;
 using static Nuke.Core.IO.FileSystemTasks;
 using static Nuke.Core.IO.PathConstruction;
+using static Nuke.Core.EnvironmentInfo;
 
 class Build : NukeBuild
 {
+    // This is the application entry point for the build.
+    // It also defines the default target to execute.
+    public static int Main() => Execute<Build>(x => x.Compile);
+
     // Auto-injection fields:
     //  - [GitVersion] must have 'GitVersion.CommandLine' referenced
     //  - [GitRepository] parses the origin from git config
@@ -17,40 +27,38 @@ class Build : NukeBuild
     //[Parameter] readonly string MyGetApiKey;
 
     Target Clean => _ => _
-        .OnlyWhen(() => false) // Disabled for safety.
-        .Executes(() =>
-        {
-            DeleteDirectories(GlobDirectories(SourceDirectory, "**/bin", "**/obj"));
-            EnsureCleanDirectory(OutputDirectory);
-        });
+            .OnlyWhen(() => false) // Disabled for safety.
+            .Executes(() =>
+            {
+                DeleteDirectories(GlobDirectories(SourceDirectory, "**/bin", "**/obj"));
+                EnsureCleanDirectory(OutputDirectory);
+            });
 
     Target Restore => _ => _
-        .DependsOn(Clean)
-        .Executes(() => { MSBuild(s => DefaultMSBuildRestore.AddRestoreSources("https://nuget.vigem.org/")); });
+            .DependsOn(Clean)
+            .Executes(() =>
+            {
+                MSBuild(s => DefaultMSBuildRestore);
+            });
 
     Target Compile => _ => _
-        .DependsOn(Restore)
-        .Executes(() =>
+            .DependsOn(Restore)
+            .Executes(() =>
         {
-            if (AppVeyor.Instance != null)
-            {
-                Logger.Info("Running on AppVeyor");
+            Logger.Info("NUKE HOST = {0}", Instance?.Host);
 
-                MSBuild(s => DefaultMSBuildCompile
-                    .SetAssemblyVersion(AppVeyor.Instance.BuildVersion)
-                    .SetFileVersion(AppVeyor.Instance.BuildVersion));
-                return;
-            }
+                if (AppVeyor.Instance != null)
+                {
+                    Logger.Info("Running on AppVeyor");
 
-            Logger.Warn("Not running on AppVeyor");
+                    MSBuild(s => DefaultMSBuildCompile
+                        .SetAssemblyVersion(AppVeyor.Instance.BuildVersion)
+                        .SetFileVersion(AppVeyor.Instance.BuildVersion));
+                    return;
+                }
 
-            MSBuild(s => DefaultMSBuildCompile);
-        });
+                Logger.Warn("Not running on AppVeyor");
 
-    // This is the application entry point for the build.
-    // It also defines the default target to execute.
-    public static int Main()
-    {
-        return Execute<Build>(x => x.Compile);
-    }
+                MSBuild(s => DefaultMSBuildCompile);
+            });
 }
