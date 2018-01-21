6f76133b4d924dfd6c788766afae1632ef2bce87
Benjamin Höglinger <nefarius@dhmx.at>
2018-01-21 09:33:24 +0100

    Added TaskQueue

---
+++ Shibari.Sub.Core/Util/TaskQueue.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Shibari.Sub.Core.Util
+{
+    public class TaskQueue
+    {
+        private readonly object _key = new object();
+        private Task _previous = Task.FromResult(false);
+
+        public Task<T> Enqueue<T>(Func<Task<T>> taskGenerator)
+        {
+            lock (_key)
+            {
+                var next = _previous.ContinueWith(t => taskGenerator()).Unwrap();
+                _previous = next;
+                return next;
+            }
+        }
+
+        public Task Enqueue(Func<Task> taskGenerator)
+        {
+            lock (_key)
+            {
+                var next = _previous.ContinueWith(t => taskGenerator()).Unwrap();
+                _previous = next;
+                return next;
+            }
+        }
+    }
+}
