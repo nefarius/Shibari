779354af28989ab86924de5c8fe0fa264216e3fc
Benjamin Höglinger <nefarius@dhmx.at>
2018-03-26 12:53:58 +0200

    Revert "Added Polly"
    This reverts commit 92311e7c11b504d79c4e3ace96371b5d27c49f20.

--- Sources/Shibari.Sub.Source.AirBender/Core/Children/DualShock3/AirBenderDualShock3.cs
+++ Sources/Shibari.Sub.Source.AirBender/Core/Children/DualShock3/AirBenderDualShock3.cs
@@ -3,7 +3,6 @@ using System.ComponentModel;
 using System.Net.NetworkInformation;
 using System.Runtime.InteropServices;
 using System.Threading;
-using Polly;
 using Serilog;
 using Shibari.Sub.Core.Shared.Types.Common;
 using Shibari.Sub.Core.Shared.Types.DualShock3;
@@ -36,7 +35,7 @@ namespace Shibari.Sub.Source.AirBender.Core.Children.DualShock3
         //
         // Values indicating which of the four LEDs to toggle
         // 
-        private readonly byte[] _ledOffsets = { 0x02, 0x04, 0x08, 0x10 };
+        private readonly byte[] _ledOffsets = {0x02, 0x04, 0x08, 0x10};
 
         public AirBenderDualShock3(AirBenderHost host, PhysicalAddress client, int index) : base(host, client, index)
         {
@@ -50,7 +49,7 @@ namespace Shibari.Sub.Source.AirBender.Core.Children.DualShock3
 
         protected override void RequestInputReportWorker(object cancellationToken)
         {
-            var token = (CancellationToken)cancellationToken;
+            var token = (CancellationToken) cancellationToken;
             var requestSize = Marshal.SizeOf<AirBenderHost.AirbenderGetDs3InputReport>();
             var requestBuffer = Marshal.AllocHGlobal(requestSize);
 
@@ -98,57 +97,45 @@ namespace Shibari.Sub.Source.AirBender.Core.Children.DualShock3
         /// <param name="l">The interval.</param>
         protected override void OnOutputReport(long l)
         {
-            Policy.Handle<PInvoke.Win32Exception>()
-                .WaitAndRetry(new[]
-                {
-                    TimeSpan.FromMilliseconds(10),
-                    TimeSpan.FromMilliseconds(20),
-                    TimeSpan.FromMilliseconds(100)
-                }, (exception, span) =>
-                {
-                    if (exception.HResult != 0xAA)
-                        throw new AirBenderSetDs3OutputReportFailedException("Sending Output Report failed.",
-                            exception);
+            var requestSize = Marshal.SizeOf<AirBenderHost.AirbenderSetDs3OutputReport>();
+            var requestBuffer = Marshal.AllocHGlobal(requestSize);
 
-                    Log.Warning("Device {ClientAddress} isn't ready yet, retrying", ClientAddress);
-                })
-                .Execute(() =>
-                {
-                    var requestSize = Marshal.SizeOf<AirBenderHost.AirbenderSetDs3OutputReport>();
-                    var requestBuffer = Marshal.AllocHGlobal(requestSize);
-
-                    try
+            try
+            {
+                //
+                // Child is identified by its unique address
+                // 
+                Marshal.StructureToPtr(
+                    new AirBenderHost.AirbenderSetDs3OutputReport
                     {
-                        //
-                        // Child is identified by its unique address
-                        // 
-                        Marshal.StructureToPtr(
-                            new AirBenderHost.AirbenderSetDs3OutputReport
-                            {
-                                ClientAddress = ClientAddress.ToNativeBdAddr(),
-                                ReportBuffer = HidOutputReport
-                            },
-                            requestBuffer, false);
+                        ClientAddress = ClientAddress.ToNativeBdAddr(),
+                        ReportBuffer = HidOutputReport
+                    },
+                    requestBuffer, false);
 
-                        var ret = HostDevice.DeviceHandle.OverlappedDeviceIoControl(
-                            AirBenderHost.IoctlAirbenderSetDs3OutputReport,
-                            requestBuffer, requestSize, IntPtr.Zero, 0,
-                            out _);
+                var ret = HostDevice.DeviceHandle.OverlappedDeviceIoControl(
+                    AirBenderHost.IoctlAirbenderSetDs3OutputReport,
+                    requestBuffer, requestSize, IntPtr.Zero, 0,
+                    out _);
 
-                        if (!ret)
-                            throw new PInvoke.Win32Exception(Marshal.GetLastWin32Error());
-                    }
-                    finally
-                    {
-                        Marshal.FreeHGlobal(requestBuffer);
-                    }
-                });
+                if (!ret)
+                    throw new AirBenderSetDs3OutputReportFailedException("Sending Output Report failed.",
+                        new PInvoke.Win32Exception(Marshal.GetLastWin32Error()));
+            }
+            catch (Exception ex)
+            {
+                Log.Error("{Exception}", ex);
+            }
+            finally
+            {
+                Marshal.FreeHGlobal(requestBuffer);
+            }
         }
 
         /// <inheritdoc />
         public override void Rumble(byte largeMotor, byte smallMotor)
         {
-            HidOutputReport[4] = (byte)(smallMotor > 0 ? 0x01 : 0x00);
+            HidOutputReport[4] = (byte) (smallMotor > 0 ? 0x01 : 0x00);
             HidOutputReport[6] = largeMotor;
 
             OnOutputReport(0);
