5f7dba37cf78cbaad9dabb60634e10436d97e84b
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2019-03-17 01:06:53 +0100

    Renamed class

---
+++ Sources/Shibari.Sub.Source.BthPS3/BthPS3BusEmulator.cs
@@ -0,0 +1,29 @@
+﻿using Shibari.Sub.Core.Shared.Types.Common;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shibari.Sub.Source.BthPS3
+{
+    [ExportMetadata("Name", "BthPS3 Bus Emulator")]
+    [Export(typeof(IBusEmulator))]
+    public class BthPS3BusEmulator : IBusEmulator
+    {
+        public event ChildDeviceAttachedEventHandler ChildDeviceAttached;
+        public event ChildDeviceRemovedEventHandler ChildDeviceRemoved;
+        public event InputReportReceivedEventHandler InputReportReceived;
+
+        public void Start()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Stop()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
