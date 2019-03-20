3e3fcf98747b965ca080d96d5b83a0cab4a8ba3a
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2019-03-20 20:14:51 +0100

    Added dependencies

--- Sources/Shibari.Sub.Source.BthPS3/BthPS3BusEmulator.cs
+++ Sources/Shibari.Sub.Source.BthPS3/BthPS3BusEmulator.cs
@@ -12,6 +12,9 @@ namespace Shibari.Sub.Source.BthPS3
     [Export(typeof(IBusEmulator))]
     public class BthPS3BusEmulator : IBusEmulator
     {
+        private readonly IObservable<long> _deviceLookupSchedule = Observable.Interval(TimeSpan.FromSeconds(2));
+        private IDisposable _deviceLookupTask;
+
         public event ChildDeviceAttachedEventHandler ChildDeviceAttached;
         public event ChildDeviceRemovedEventHandler ChildDeviceRemoved;
         public event InputReportReceivedEventHandler InputReportReceived;
