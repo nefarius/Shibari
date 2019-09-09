dce8ef06bc6f902cdd887785c2587c5da68ad3e9
Benjamin Höglinger-Stelzer <benjamin.hoeglinger@nefarius.at>
2019-09-09 15:03:05 +0200

    Updated DS4 emulation to use new API

--- Sinks/Shibari.Sub.Sink.ViGEm/Core/ViGEmSink.cs
+++ Sinks/Shibari.Sub.Sink.ViGEm/Core/ViGEmSink.cs
@@ -1,4 +1,4 @@
-﻿#define X360
+﻿#define DS4
 
 using System;
 using System.Collections.Generic;
@@ -6,6 +6,7 @@ using System.ComponentModel.Composition;
 using System.Linq;
 using Nefarius.ViGEm.Client;
 using Nefarius.ViGEm.Client.Targets;
+using Nefarius.ViGEm.Client.Targets.DualShock4;
 using Nefarius.ViGEm.Client.Targets.Xbox360;
 using Serilog;
 using Shibari.Sub.Core.Shared.Types.Common;
@@ -19,28 +20,50 @@ namespace Shibari.Sub.Sink.ViGEm.Core
     [Export(typeof(ISinkPlugin))]
     public class ViGEmSink : ISinkPlugin
     {
-        private readonly Dictionary<DualShock3Buttons, DualShock4Buttons> _btnMap;
+        private readonly Dictionary<DualShock3Buttons, DualShock4Button> _btnMap;
         private readonly ViGEmClient _client;
 
-        private readonly Dictionary<IDualShockDevice, DualShock4Controller> _deviceMap =
-            new Dictionary<IDualShockDevice, DualShock4Controller>();
+        private readonly Dictionary<IDualShockDevice, IDualShock4Controller> _deviceMap =
+            new Dictionary<IDualShockDevice, IDualShock4Controller>();
+
+        private readonly Dictionary<DualShock3Axes, DualShock4Slider> _triggerAxisMap;
+        private readonly Dictionary<DualShock3Axes, DualShock4Axis> _XaxisMap;
+        private readonly Dictionary<DualShock3Axes, DualShock4Axis> _YaxisMap;
 
         public ViGEmSink()
         {
-            _btnMap = new Dictionary<DualShock3Buttons, DualShock4Buttons>
+            _btnMap = new Dictionary<DualShock3Buttons, DualShock4Button>
             {
-                {DualShock3Buttons.Select, DualShock4Buttons.Share},
-                {DualShock3Buttons.LeftThumb, DualShock4Buttons.ThumbLeft},
-                {DualShock3Buttons.RightThumb, DualShock4Buttons.ThumbRight},
-                {DualShock3Buttons.Start, DualShock4Buttons.Options},
-                {DualShock3Buttons.LeftTrigger, DualShock4Buttons.TriggerLeft},
-                {DualShock3Buttons.RightTrigger, DualShock4Buttons.TriggerRight},
-                {DualShock3Buttons.LeftShoulder, DualShock4Buttons.ShoulderLeft},
-                {DualShock3Buttons.RightShoulder, DualShock4Buttons.ShoulderRight},
-                {DualShock3Buttons.Triangle, DualShock4Buttons.Triangle},
-                {DualShock3Buttons.Circle, DualShock4Buttons.Circle},
-                {DualShock3Buttons.Cross, DualShock4Buttons.Cross},
-                {DualShock3Buttons.Square, DualShock4Buttons.Square}
+                {DualShock3Buttons.Select, DualShock4Button.Share},
+                {DualShock3Buttons.LeftThumb, DualShock4Button.ThumbLeft},
+                {DualShock3Buttons.RightThumb, DualShock4Button.ThumbRight},
+                {DualShock3Buttons.Start, DualShock4Button.Options},
+                {DualShock3Buttons.LeftTrigger, DualShock4Button.TriggerLeft},
+                {DualShock3Buttons.RightTrigger, DualShock4Button.TriggerRight},
+                {DualShock3Buttons.LeftShoulder, DualShock4Button.ShoulderLeft},
+                {DualShock3Buttons.RightShoulder, DualShock4Button.ShoulderRight},
+                {DualShock3Buttons.Triangle, DualShock4Button.Triangle},
+                {DualShock3Buttons.Circle, DualShock4Button.Circle},
+                {DualShock3Buttons.Cross, DualShock4Button.Cross},
+                {DualShock3Buttons.Square, DualShock4Button.Square}
+            };
+
+            _XaxisMap = new Dictionary<DualShock3Axes, DualShock4Axis>
+            {
+                {DualShock3Axes.LeftThumbX, DualShock4Axis.LeftThumbX},
+                {DualShock3Axes.RightThumbX, DualShock4Axis.RightThumbX}
+            };
+
+            _YaxisMap = new Dictionary<DualShock3Axes, DualShock4Axis>
+            {
+                {DualShock3Axes.LeftThumbY, DualShock4Axis.LeftThumbY},
+                {DualShock3Axes.RightThumbY, DualShock4Axis.RightThumbY}
+            };
+
+            _triggerAxisMap = new Dictionary<DualShock3Axes, DualShock4Slider>
+            {
+                {DualShock3Axes.LeftTrigger, DualShock4Slider.LeftTrigger},
+                {DualShock3Axes.RightTrigger, DualShock4Slider.RightTrigger}
             };
 
             _client = new ViGEmClient();
@@ -52,7 +75,8 @@ namespace Shibari.Sub.Sink.ViGEm.Core
         {
             Log.Information("Device {Device} got attached", device);
 
-            var target = new DualShock4Controller(_client);
+            var target = _client.CreateDualShock4Controller();
+            target.AutoSubmitReport = false;
 
             _deviceMap.Add(device, target);
 
@@ -79,7 +103,7 @@ namespace Shibari.Sub.Sink.ViGEm.Core
         {
             Log.Information("Device {Device} got removed", device);
 
-            _deviceMap[device].Dispose();
+            _deviceMap[device].Disconnect();
             _deviceMap.Remove(device);
         }
 
@@ -90,46 +114,42 @@ namespace Shibari.Sub.Sink.ViGEm.Core
                 case DualShockDeviceType.DualShock3:
 
                     var target = _deviceMap[device];
+                    target.ResetReport();
 
                     var ds3Report = (DualShock3InputReport)report;
-                    var ds4Report = new DualShock4Report();
 
-                    ds4Report.SetAxis(DualShock4Axes.LeftThumbX, ds3Report[DualShock3Axes.LeftThumbX]);
-                    ds4Report.SetAxis(DualShock4Axes.LeftThumbY, ds3Report[DualShock3Axes.LeftThumbY]);
-                    ds4Report.SetAxis(DualShock4Axes.RightThumbX, ds3Report[DualShock3Axes.RightThumbX]);
-                    ds4Report.SetAxis(DualShock4Axes.RightThumbY, ds3Report[DualShock3Axes.RightThumbY]);
-                    ds4Report.SetAxis(DualShock4Axes.LeftTrigger, ds3Report[DualShock3Axes.LeftTrigger]);
-                    ds4Report.SetAxis(DualShock4Axes.RightTrigger, ds3Report[DualShock3Axes.RightTrigger]);
+                    foreach (var axis in _XaxisMap) target.SetAxisValue(axis.Value, ds3Report[axis.Key]);
 
-                    ds4Report.SetButtons(_btnMap.Where(m => ds3Report.EngagedButtons.Contains(m.Key))
-                        .Select(m => m.Value).ToArray());
+                    foreach (var axis in _YaxisMap) target.SetAxisValue(axis.Value, ds3Report[axis.Key]);
+
+                    foreach (var axis in _triggerAxisMap) target.SetSliderValue(axis.Value, ds3Report[axis.Key]);
+
+                    foreach (var button in _btnMap.Where(m => ds3Report.EngagedButtons.Contains(m.Key))
+                        .Select(m => m.Value)) target.SetButtonState(button, true);
 
                     if (ds3Report.EngagedButtons.Contains(DualShock3Buttons.DPadUp))
-                        ds4Report.SetDPad(DualShock4DPadValues.North);
+                        target.SetDPadDirection(DualShock4DPadDirection.North);
                     if (ds3Report.EngagedButtons.Contains(DualShock3Buttons.DPadRight))
-                        ds4Report.SetDPad(DualShock4DPadValues.East);
+                        target.SetDPadDirection(DualShock4DPadDirection.East);
                     if (ds3Report.EngagedButtons.Contains(DualShock3Buttons.DPadDown))
-                        ds4Report.SetDPad(DualShock4DPadValues.South);
+                        target.SetDPadDirection(DualShock4DPadDirection.South);
                     if (ds3Report.EngagedButtons.Contains(DualShock3Buttons.DPadLeft))
-                        ds4Report.SetDPad(DualShock4DPadValues.West);
+                        target.SetDPadDirection(DualShock4DPadDirection.West);
 
                     if (ds3Report.EngagedButtons.Contains(DualShock3Buttons.DPadUp)
                         && ds3Report.EngagedButtons.Contains(DualShock3Buttons.DPadRight))
-                        ds4Report.SetDPad(DualShock4DPadValues.Northeast);
+                        target.SetDPadDirection(DualShock4DPadDirection.Northeast);
                     if (ds3Report.EngagedButtons.Contains(DualShock3Buttons.DPadRight)
                         && ds3Report.EngagedButtons.Contains(DualShock3Buttons.DPadDown))
-                        ds4Report.SetDPad(DualShock4DPadValues.Southeast);
+                        target.SetDPadDirection(DualShock4DPadDirection.Southeast);
                     if (ds3Report.EngagedButtons.Contains(DualShock3Buttons.DPadDown)
                         && ds3Report.EngagedButtons.Contains(DualShock3Buttons.DPadLeft))
-                        ds4Report.SetDPad(DualShock4DPadValues.Southwest);
+                        target.SetDPadDirection(DualShock4DPadDirection.Southwest);
                     if (ds3Report.EngagedButtons.Contains(DualShock3Buttons.DPadLeft)
                         && ds3Report.EngagedButtons.Contains(DualShock3Buttons.DPadUp))
-                        ds4Report.SetDPad(DualShock4DPadValues.Northwest);
+                        target.SetDPadDirection(DualShock4DPadDirection.Northwest);
 
-                    if (ds3Report.EngagedButtons.Contains(DualShock3Buttons.Ps))
-                        ds4Report.SetSpecialButtons(DualShock4SpecialButtons.Ps);
-
-                    target.SendReport(ds4Report);
+                    target.SubmitReport();
 
                     break;
             }
