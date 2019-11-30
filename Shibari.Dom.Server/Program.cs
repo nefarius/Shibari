d6d10f74ec6fed8c426fcbfc4cb4b0c5fec95d14
Benjamin Höglinger-Stelzer <benjamin.hoeglinger@nefarius.at>
2019-11-30 13:31:49 +0100

    Implemented self-unblocking

--- Shibari.Dom.Server/Program.cs
+++ Shibari.Dom.Server/Program.cs
@@ -1,9 +1,13 @@
 ﻿using System;
 using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
 using System.Threading;
 using Serilog;
 using Shibari.Dom.Server.Core;
 using Topshelf;
+using Trinet.Core.IO.Ntfs;
 
 namespace Shibari.Dom.Server
 {
@@ -26,8 +30,27 @@ namespace Shibari.Dom.Server
 
             AppDomain.CurrentDomain.UnhandledException += (sender, eventArgs) =>
                 {
-                    Log.Fatal("Unhandled exception: {Exception}", (Exception) eventArgs.ExceptionObject);
+                    Log.Fatal("Unhandled exception: {Exception}", (Exception)eventArgs.ExceptionObject);
                 };
+
+            try
+            {
+                var domRoot = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) ?? ".";
+                var files = Directory.GetFiles(domRoot, "*.dll", SearchOption.AllDirectories);
+
+                foreach (var fileInfo in files.Select(f => new FileInfo(f)))
+                {
+                    if (!fileInfo.AlternateDataStreamExists("Zone.Identifier")) continue;
+                    Log.Information("Removing Zone.Identifier from file {File}", fileInfo.Name);
+                    var ads = fileInfo.GetAlternateDataStream("Zone.Identifier", FileMode.Open);
+                    ads.Delete();
+                }
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal("Error unblocking files, program may be unusable, contact support!");
+                return;
+            }
 
             HostFactory.Run(x =>
             {
